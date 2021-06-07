02ac2c73da79b315f1e26a33f87c6e52c1c2eb2b
kimlimjustin <kimlimjustin@gmail.com>
2021-06-07 12:58:24 +0700

    fix: wrong placement of requiring file code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { isHiddenFile: checkHiddenFileWin } = require("./binding");
 
 const isHiddenFile = (path) => {
     switch (process.platform) {
         case "win32":
+            const { isHiddenFile: checkHiddenFileWin } = require("./binding");
             return checkHiddenFileWin(path)
         default:
             return /(^|\/)\.[^\/\.]/g.test(path)
