08b3c10237977dd6671e7ae6644d618251ff4b4c
kimlimjustin <kimlimjustin@gmail.com>
2021-06-08 14:03:58 +0700

    feat: add jsdoc and typescript declare module

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Check whether a file/folder is hidden on system
+ * @author Justin Maximillian Kimlim <kimlimjustin@gmail.com>
+ * @license MIT
+ * @param {string} path - file/folder absolute path
+ * @returns {boolean} Result
+*/
+export function isHiddenFile(path: string): boolean;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 
+/**
+ * Check whether a file/folder is hidden on system
+ * @author Justin Maximillian Kimlim <kimlimjustin@gmail.com>
+ * @license MIT
+ * @param {string} path - file/folder absolute path
+ * @returns {boolean} Result
+*/
+
 const isHiddenFile = (path) => {
     switch (process.platform) {
         case "win32":
